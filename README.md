99db318abe6e9271d26b3a2e973c8adb4d83a37a
AnthonyMancia3670 <amancia2005@gmail.com>
2025-01-30 04:24:42 -0500

    Added the AlertDialog

---
+++ README.md
@@ -0,0 +1,3 @@
+#Anthony Mancia 
+
+##The purpose of the project is to test UI using the sampleview
--- anthonyView3/src/main/java/com/example/simpleviews1/MainActivity.java
+++ anthonyView3/src/main/java/com/example/simpleviews1/MainActivity.java
@@ -1,8 +1,10 @@
 //Anthony Mancia n01643670
 package com.example.simpleviews1;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,7 +17,7 @@ import android.widget.ToggleButton;
 public class MainActivity extends AppCompatActivity {
 
     public void btnSaved_clicked (View view) {
-        DisplayToast("You have clicked the Save button1");
+        DisplayToast(getString(R.string.Save_btn_Clicked));
     }
 
     /** Called when the activity is first created. */
@@ -25,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+
+
         //---Button view---
         Button btnOpen = (Button) findViewById(R.id.btnOpen);
         //register the button with an event listener
@@ -32,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         { //anonymous class
             //implement your event handler method
             public void onClick(View v) {
-                DisplayToast("You have clicked the Open button");
+                DisplayToast(getString(R.string.Open_btn_Clicked));
             }
         });
 
@@ -53,9 +57,9 @@ public class MainActivity extends AppCompatActivity {
         {
             public void onClick(View v) {
                 if (((CheckBox)v).isChecked())
-                    DisplayToast("CheckBox is checked");
+                    DisplayToast(getString(R.string.Checkbox_Checked));
                 else
-                    DisplayToast("CheckBox is unchecked");
+                    DisplayToast(getString(R.string.Checkbox_Unchecked));
             }
         });
 
@@ -66,9 +70,9 @@ public class MainActivity extends AppCompatActivity {
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 RadioButton rb1 = (RadioButton) findViewById(R.id.rdb1);
                 if (rb1.isChecked()) {
-                    DisplayToast("Option 1 checked!");
+                    DisplayToast(getString(R.string.Option1Checked));
                 } else {
-                    DisplayToast("Option 2 checked!");
+                    DisplayToast(getString(R.string.Option2Checked));
                 }
             }
         });
@@ -80,11 +84,33 @@ public class MainActivity extends AppCompatActivity {
         {
             public void onClick(View v) {
                 if (((ToggleButton)v).isChecked())
-                    DisplayToast("Toggle button is On");
+                    DisplayToast(getString(R.string.Toggle_On));
                 else
-                    DisplayToast("Toggle button is Off");
+                    DisplayToast(getString(R.string.Toggle_Off));
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.dialog_title));
+        builder.setMessage(getString(R.string.dialog_message));
+
+        // Use a valid icon from the drawable folder (ensure the icon exists)
+        builder.setIcon(R.drawable.exitsign);
+
+        // Yes Button - Exit App
+        builder.setPositiveButton(getString(R.string.yes_button), (dialog, which) -> finish());
+
+        // No Button - Stay in App
+        builder.setNegativeButton(getString(R.string.no_button), (dialog, which) -> dialog.dismiss());
+
+        // Prevent user from dismissing the dialog without answering
+        builder.setCancelable(false);
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 
     private void DisplayToast(String msg)
